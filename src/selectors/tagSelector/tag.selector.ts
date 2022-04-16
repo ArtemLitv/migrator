e1ac704d605c4a9272ecd12fd44f0a1a7e52df3f
ArtemLitv <artem96_92@mail.ru>
2022-04-16 20:10:47 +0300

    init commit

--- src/selectors/tagSelector/tag.selector.ts
+++ src/selectors/tagSelector/tag.selector.ts
@@ -1,19 +1,22 @@
 import { Rule } from '../../models/rule';
 import { log } from '../../services/logger.service';
-import { getCurrentSelectorData, next } from '../../utils/nextSelector.util';
+import { getCurrentSelectorData } from '../../utils/nextSelector.util';
 
-export const TAG = (rule: Rule) => (match: string): string => {
-	const activeSelector = getCurrentSelectorData(rule);
-	log(`↳ ${activeSelector.rule}`, { isCode: true });
+export const TAG =
+	(rule: Rule) =>
+	(match: string): string => {
+		const activeSelector = getCurrentSelectorData(rule);
+		log(`↳ ${activeSelector.rule}`, { isCode: true });
 
-	// await next(rule, match);
-	// TODO: тут должен быть вызов либо другого селектора, либо rule. Используется как тестовый вариант
-	return match.toUpperCase();
-};
-
+		// await next(rule, match);
+		// TODO: тут должен быть вызов либо другого селектора, либо rule. Используется как тестовый вариант
+		return match.toUpperCase();
+	};
 
 export const TAG_REGEXP = (rule: Rule): RegExp => {
 	const activeSelector = getCurrentSelectorData(rule);
 	const tag: string = activeSelector.rule.replace('TAG ', '');
-	return new RegExp(tag, 'g');
+	const template = `<${tag}[\\w\\W]*>[\\w\\W]*<\\/${tag}>`;
+	const result = new RegExp(template, 'g');
+	return result;
 };
